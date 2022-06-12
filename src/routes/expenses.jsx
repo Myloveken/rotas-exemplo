1850c2e63d21e1b2e1f01618eab13daed39cef58
Kenlove Chereste <cherestek@gmail.com>
2022-06-12 19:06:37 -0300

    Desafio do projeto

---
+++ src/routes/expenses.jsx
@@ -0,0 +1,12 @@
+export default function Expenses() {
+    return (
+
+        <
+        main >
+        <
+        h3 style = {
+            { padding: "1rem 0" }
+        } > Despesas < /h3> <
+        /main>
+    )
+}
